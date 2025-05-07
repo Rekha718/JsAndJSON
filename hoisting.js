5ce62c6aeebcc6d7439abd46d3f56af55d94aae9
Rekha718 <rekha2002718@gmail.com>
2025-05-07 17:07:51 +0530

    dom methods

---
+++ hoisting.js
@@ -0,0 +1,14 @@
+//variable hoisting 
+x=10
+console.log(x)
+
+console.log(a); //undefined
+var a = 10;
+var x;
+
+//function hositing
+myFunction()
+
+function myFunction() {
+    console.log("Hello World!")
+}
